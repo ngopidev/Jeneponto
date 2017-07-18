c485e76470b6ed94f5c8d4cbdecf0d3f331d6aa3
androbita <pranadana55@gmail.com>
2017-07-19 00:59:59 +0700

    iseng

--- app/src/main/java/com/ngopi/jeneponto/SplashScreen.java
+++ app/src/main/java/com/ngopi/jeneponto/SplashScreen.java
@@ -9,5 +9,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_splash);
+
+        onDestroy();
     }
 }
